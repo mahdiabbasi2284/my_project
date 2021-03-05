6e0107deaba7ec64c68c285488a284e0531e4c59
mahdiabbasi2284 <mahdi.abbasi2284@gmail.com>
2021-03-05 22:19:55 +0330

    Initial commit

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.my_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1,0);
+        alphaAnimation.setDuration(3000);
+
+        ImageView imageView = findViewById(R.id.iv_main);
+        View playButton = findViewById(R.id.fab_main);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.startAnimation(alphaAnimation);
+            }
+        });
+
     }
 }
