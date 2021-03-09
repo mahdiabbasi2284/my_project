b4725d727d825e8aa32dc0b0da2f11e79b7adece
mahdiabbasi2284 <mahdi.abbasi2284@gmail.com>
2021-03-09 22:05:58 +0330

    scale animation

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,16 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AlphaAnimation alphaAnimation = new AlphaAnimation(1,0);
-        alphaAnimation.setDuration(3000);
-        alphaAnimation.setFillAfter(true);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1,2,1,2,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        scaleAnimation.setDuration(1000);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setRepeatCount(Animation.INFINITE);
+        scaleAnimation.setRepeatMode(Animation.REVERSE);
+//        AlphaAnimation alphaAnimation = new AlphaAnimation(1,0);
+//        alphaAnimation.setDuration(3000);
+//        alphaAnimation.setRepeatCount(Animation.INFINITE);
+//        //alphaAnimation.setFillAfter(true);
         ImageView imageView = findViewById(R.id.iv_main);
         View playButton = findViewById(R.id.fab_main);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageView.startAnimation(alphaAnimation);
+                imageView.startAnimation(scaleAnimation);
             }
         });
 
