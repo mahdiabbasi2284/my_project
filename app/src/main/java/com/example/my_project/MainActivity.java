5fa9f4e5d182778ad0d562081de0d99f07e309bd
mahdiabbasi2284 <mahdi.abbasi2284@gmail.com>
2021-03-11 18:43:43 +0330

    Rotate Animation

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,22 +24,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ScaleAnimation scaleAnimation = new ScaleAnimation(1,2,1,2,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        RotateAnimation rotateAnimation = new RotateAnimation(0,
+                360,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+        rotateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        rotateAnimation.setDuration(1000);
+        /*TranslateAnimation translateAnimation = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,
+                0,
+                Animation.RELATIVE_TO_PARENT,
+                0,
+                Animation.RELATIVE_TO_PARENT,
+                0,
+                Animation.RELATIVE_TO_PARENT,
+                0.5f);ScaleAnimation scaleAnimation = new ScaleAnimation(1,2,1,2,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
         scaleAnimation.setDuration(1000);
         scaleAnimation.setFillAfter(true);
         scaleAnimation.setRepeatCount(Animation.INFINITE);
         scaleAnimation.setRepeatMode(Animation.REVERSE);
-//        AlphaAnimation alphaAnimation = new AlphaAnimation(1,0);
-//        alphaAnimation.setDuration(3000);
-//        alphaAnimation.setRepeatCount(Animation.INFINITE);
-//        //alphaAnimation.setFillAfter(true);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1,0);
+        alphaAnimation.setDuration(3000);
+        alphaAnimation.setRepeatCount(Animation.INFINITE);
+        //alphaAnimation.setFillAfter(true)
+        translateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        translateAnimation.setDuration(1000);
+        translateAnimation.setFillAfter(true);*/
         ImageView imageView = findViewById(R.id.iv_main);
         View playButton = findViewById(R.id.fab_main);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageView.startAnimation(scaleAnimation);
+                imageView.startAnimation(rotateAnimation);
             }
         });
 
